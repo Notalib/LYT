d540b50fda9be21e3b381afd451fe1059cf13045
Michael Zedeler <michael@zedeler.dk>
2012-10-04 09:23:13 +0200

    Upgraded to jQuery Mobile router 0.93.

--- assets/lib/router/jquery.mobile.router.js
+++ assets/lib/router/jquery.mobile.router.js
@@ -1,7 +1,19 @@
-/* jQueryMobile-router v0.6
- * Copyright 2011, Andrea Zicchetti
+/*!
+ * jQueryMobile-router v0.93
+ * http://github.com/azicchetti/jquerymobile-router
+ *
+ * Copyright 2011 (c) Andrea Zicchetti
+ * Dual licensed under the MIT or GPL Version 2 licenses.
+ * http://github.com/azicchetti/jquerymobile-router/blob/master/MIT-LICENSE.txt
+ * http://github.com/azicchetti/jquerymobile-router/blob/master/GPL-LICENSE.txt
  */
-(function($){
+(function (root, factory) {
+  if (typeof define === 'function' && define.amd) {
+    define(['jquery'], factory);
+  } else {
+    factory(jQuery);
+  }
+}(this, function ($) {
 
 $(document).bind("mobileinit",function(){
 
@@ -19,7 +31,8 @@ $(document).bind("mobileinit",function(){
 	*/
 
 	var config=$.extend({
-		fixFirstPageDataUrl: false, firstPageDataUrl: "index.html", ajaxApp: false
+		fixFirstPageDataUrl: false, firstPageDataUrl: "index.html",
+		ajaxApp: false, firstMatchOnly: false
 	},$.mobile.jqmRouter || {});
 
 
@@ -31,43 +44,50 @@ $(document).bind("mobileinit",function(){
 	var previousUrl=null, nextUrl=null;
 
 	$(document).bind("pagebeforechange", function( e, data ) {
-		// We only want to handle changePage() calls where the caller is
-		// asking us to load a page by URL.
-		if ( typeof data.toPage === "string" ) {
-			// We are being asked to load a page by URL, but we only
-			// want to handle URLs that request the data for a specific
-			// category.
-			var u = $.mobile.path.parseUrl( data.toPage );
-			previousUrl=nextUrl;
-			nextUrl=u;
+		var toPage=( typeof data.toPage === "string" ) ? data.toPage : data.toPage.jqmData("url")||"";
 
-			if ( u.hash.indexOf("?") !== -1 ) {
-				var page=u.hash.replace( /\?.*$/, "" );
-				// We don't want the data-url of the page we just modified
-				// to be the url that shows up in the browser's location field,
-				// so set the dataUrl option to the URL with hash parameters
-				data.options.dataUrl = u.href;
-				// Now call changePage() and tell it to switch to
-				// the page we just modified, but only in case it's different
-				// from the current page
-				if (	$.mobile.activePage &&
-					page.replace(/^#/,"")==$.mobile.activePage.jqmData("url")
-				){
-					data.options.allowSamePageTransition=true;
-					$.mobile.changePage( $(page), data.options );
-				} else {
-					$.mobile.changePage( $(page), data.options );
-				}
-				// Make sure to tell changePage() we've handled this call so it doesn't
-				// have to do anything.
-				e.preventDefault();
+		if ( data.options.hasOwnProperty("_jqmrouter_handled") ){ return; }
+		data.options._jqmrouter_handled = true;
+		// handle form submissions
+		if (data.options.data && (data.options.type+"").toLowerCase()=="get"){
+			toPage+="?"+data.options.data;
+		}
+		var u = $.mobile.path.parseUrl( toPage );
+		previousUrl=nextUrl;
+		nextUrl=u;
+
+		if ( 	u.hash.indexOf("?") !== -1 ||
+			(u.hash.length>0 && previousUrl!==null && previousUrl.hash.indexOf(u.hash+"?")!==-1)
+		) {
+			var page=u.hash.replace( /\?.*$/, "" );
+			// We don't want the data-url of the page we just modified
+			// to be the url that shows up in the browser's location field,
+			// so set the dataUrl option to the URL with hash parameters
+			data.options.dataUrl = u.href;
+			// Now call changePage() and tell it to switch to
+			// the page we just modified, but only in case it's different
+			// from the current page
+			if (	$.mobile.activePage &&
+				page.replace(/^#/,"")==$.mobile.activePage.jqmData("url")
+			){
+				data.options.allowSamePageTransition=true;
+				$.mobile.changePage( $(page), data.options );
+			} else {
+				$.mobile.changePage( $(page), data.options );
 			}
+			// Make sure to tell changePage() we've handled this call so it doesn't
+			// have to do anything.
+			e.preventDefault();
+			$.mobile.urlHistory.ignoreNextHashChange=true;
 		}
 	});
 
 
 	if (config.fixFirstPageDataUrl){
 		$(document).ready(function(){
+			if (!window.location.pathname.match("/$")){
+				return;
+			}			
 			var page=$(":jqmData(role='page')").first();
 			var	dataUrl=page.jqmData("url"),
 				guessedDataUrl=window.location.pathname
@@ -75,9 +95,7 @@ $(document).bind("mobileinit",function(){
 					+window.location.search
 					+window.location.hash
 			;
-			if (!window.location.pathname.match("/$")){
-				return;
-			}
+
 			if (dataUrl!=guessedDataUrl){
 				page.attr("data-url",guessedDataUrl)
 					.jqmData("url",guessedDataUrl);
@@ -98,9 +116,13 @@ $(document).bind("mobileinit",function(){
 			pagebeforecreate: null, pagecreate: null,
 			pagebeforeshow: null, pageshow: null,
 			pagebeforehide: null, pagehide: null,
-			pageinit: null, pageremove: null
+			pageinit: null, pageremove: null,
+			pagebeforechange: null, pagebeforeload: null,
+			pageload: null
 		};
 		this.evtLookup = {
+			bC: "pagebeforechange", bl: "pagebeforeload",
+			l: "pageload",
 			bc: "pagebeforecreate", c: "pagecreate",
 			bs: "pagebeforeshow", s: "pageshow",
 			bh: "pagebeforehide", h: "pagehide",
@@ -118,13 +140,15 @@ $(document).bind("mobileinit",function(){
 		this.add(userRoutes,userHandlers);
 	}
 	$.extend($.mobile.Router.prototype,{
-		add: function(userRoutes,userHandlers){
+		documentEvts: { pagebeforechange:1, pagebeforeload:1, pageload:1 },
+
+		add: function(userRoutes,userHandlers,_skipAttach){
 			if (!userRoutes) return;
 
-			var _self=this, evtList=[];
+			var _self=this, evtList=[], docEvtList=[];
 			if (userRoutes instanceof Array){
 				$.each(userRoutes,$.proxy(function(k,v){
-					this.add(v,userHandlers);
+					this.add(v,userHandlers,true);
 				},this));
 			} else {
 				$.each(userRoutes,function(r,el){
@@ -138,7 +162,7 @@ $(document).bind("mobileinit",function(){
 						}
 					} else {
 						var i,trig=el.events.split(","),evt;
-						for(i in trig){
+						for(i=0;i<trig.length;i++){
 							evt=_self.evtLookup[trig[i]];
 							if (_self.routes.hasOwnProperty(evt)){
 								if (_self.routes[evt]===null){
@@ -154,23 +178,41 @@ $(document).bind("mobileinit",function(){
 						}
 					}
 				});
-				$.each(_self.routes,function(evt,el){
-					if (el!==null){
-						evtList.push(evt);
-					}
-				});
-				if (!this.userHandlers) this.userHandlers={};
+			}
+			if (_skipAttach===true) return;
+			if (!this.userHandlers){
+				this.userHandlers=userHandlers||{};
+			} else {
 				$.extend(this.userHandlers,userHandlers||{});
-				this._detachEvents();
-				if (evtList.length>0){
-					this._liveData={
-						events: evtList.join(" "),
-						handler: function(e,ui){ _self._processRoutes(e,ui,this); }
-					};
-					$("div:jqmData(role='page'),div:jqmData(role='dialog')").live(
-						this._liveData.events, this._liveData.handler
-					);
+			}
+			$.each(_self.routes,function(evt,el){
+				if (el!==null){
+					if (!_self.documentEvts[evt]){
+						evtList.push(evt);
+					} else {
+						docEvtList.push(evt);
+					}
 				}
+			});
+			this._detachEvents();
+			var routeHandler=function(e,ui){ _self._processRoutes(e,ui,this); };
+			if (evtList.length>0){
+				this._eventData={
+					events: evtList.join(" "),
+					selectors: ":jqmData(role='page'),:jqmData(role='dialog')",
+					handler: routeHandler
+				};
+				$(document).delegate(
+					this._eventData.selectors,
+					this._eventData.events, this._eventData.handler
+				);
+			}
+			if (docEvtList.length>0){
+				this._docEventData={
+					events: docEvtList.join(" "),
+					handler: routeHandler
+				};
+				$(document).bind(this._docEventData.events, this._docEventData.handler);
 			}
 		},
 
@@ -193,7 +235,7 @@ $(document).bind("mobileinit",function(){
 							refUrl=$.mobile.path.parseUrl(refUrl);
 						}
 					}
-				} else if (page && !$(page).jqmData("url")){
+				} else if (!this.documentEvts[e.type] && page && !$(page).jqmData("url")){
 					return;
 				}
 				if (!refUrl) return;
@@ -220,27 +262,32 @@ $(document).bind("mobileinit",function(){
 						handleFn=_self.userHandlers[handler];
 					}
 					if (handleFn){
-						try { handleFn(e.type,res,ui,page); bHandled = true;
-						}catch(err){ debug("each" + err); }//debug("in each" + err +" handle " + handleFn ); }
-						 //handel broken handlers
+						try { handleFn.apply(_self.userHandlers, [e.type,res,ui,page,e]);
+							bHandled = true;
+						}catch(err){ debug(err); }
 					}
 				}
+				if (bHandled && _self.conf.firstMatchOnly) return false;
 			});
 			//Pass to default if specified and can handle this event type
 			if (!bHandled && this.conf.defaultHandler && this.defaultHandlerEvents[e.type]) {
 				if (typeof(this.conf.defaultHandler) == "function") {
 					try {
-						this.conf.defaultHandler(e.type, ui, page);
-					} catch(err) { debug("default" + err); }
+						this.conf.defaultHandler.apply(_self.userHandlers, [e.type, ui, page, e]);
+					} catch(err) { debug(err); }
 				}
 			}
 		},
 
 		_detachEvents: function(){
-			if (this._liveData){
-				$("div:jqmData(role='page'),div:jqmData(role='dialog')").die(
-					this._liveData.events, this._liveData.handler
+			if (this._eventData){
+				$(document).undelegate(
+					this._eventData.selectors,
+					this._eventData.events, this._eventData.handler
 				);
+			}
+			if (this._docEventData){
+				$(document).unbind(this._docEventData.events, this._docEventData.handler);
 			}
 		} ,
 
@@ -255,13 +302,14 @@ $(document).bind("mobileinit",function(){
 			var tokens=hashparams.slice( hashparams.indexOf('?')+1 ).split("&");
 			$.each(tokens,function(k,v){
 				tmp=v.split("=");
+				tmp[0]=decodeURIComponent(tmp[0]);
 				if (params[tmp[0]]){
 					if (!(params[tmp[0]] instanceof Array)){
 						params[tmp[0]]=[ params[tmp[0]] ];
 					}
-					params[tmp[0]].push(tmp[1]);
+					params[tmp[0]].push( decodeURIComponent(tmp[1]) );
 				} else {
-					params[tmp[0]]=tmp[1];
+					params[tmp[0]]=decodeURIComponent(tmp[1]);
 				}
 			});
 			if ($.isEmptyObject(params)) return null;
@@ -270,5 +318,6 @@ $(document).bind("mobileinit",function(){
 	});
 
 });
+return {};
 
-})(jQuery);
+}));
