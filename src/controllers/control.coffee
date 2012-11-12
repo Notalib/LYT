Notalib/LYT

commit e4c21c273c05548f691975141c6ed05fd4e9047b

Fixed conflicts in files

src/controllers/control.coffee
@@ -158,21 +158,9 @@ LYT.control =
             log.message "Control: bookDetails: failed with error #{error} and msg #{msg}"
           .done (details) ->
             LYT.render.bookDetails(details, content)
-<<<<<<< HEAD
         
         LYT.loader.register "Loading book", process
   
-=======
-            content.children().show()
-            content.find("#add-to-bookshelf-button").bind "click", (event) ->
-              # TODO: This is far from perfect: There's no way
-              # of knowing if something's already on the shelf
-              LYT.loader.register "Adding book to bookshelf", LYT.bookshelf.add(params.book).done( -> $.mobile.changePage "#bookshelf" )
-              $(this).unbind event 
-              event.preventDefault()
-              event.stopImmediatePropagation()
- 
->>>>>>> 0363e047f87431d65b8fc7d6fb6ae678ff7c7f52
   # TODO: Move bookmarks list to separate page
   # TODO: Bookmarks and toc does not work properly after a forced refresh on the #book-index page. Needs to be fixed when force reloading the entire app.
   bookIndex: (type, match, ui, page, event) ->

html/pages/search.html
@@ -9,21 +9,12 @@
 		
 		<div id="search-form-wrapper">
     		<form action="#" method="get" id="search-form">	
-<<<<<<< HEAD
     			<fieldset >
 				  <div class="ui-grid-solo">
     			    <div class="ui-block-a">
     				  <label for="term" class="ui-hidden-accessible">Søg:</label>
     					<input type="search" name="term" id="searchterm" placeholder="Søg på titel, forfatter eller genre" title="Søg på titel, forfatter eller genre"/>
               		</div>
-=======
-    			<fieldset>
-					<div class="ui-grid-solo">
-	    				<div class="ui-block-a">
-	    					<label for="term" class="ui-hidden-accessible">Søg:</label>
-	    					<input type="search" name="term" id="searchterm" placeholder="Søg på titel, forfatter eller genre" title="Søg på titel, forfatter eller genre"/>
-	              		</div>
->>>>>>> 0363e047f87431d65b8fc7d6fb6ae678ff7c7f52
 					</div>
 	               	<div id="search-icon-btn">
 	                   <input class="gatrack" ga-action="Søg-knap" data-inline="true" type="submit" name="search-submit" id="search-submit" data-iconpos="notext" data-icon="lyt-search-main" data-iconshadow="false" value="søg"/>

commit 382150ac1072d29b0a9b4bfc7191cc63a2c34682

Reapplied lines stemming from issue #391 (broken in commit e4c21c273c05548f691975141c6ed05fd4e9047b).

src/controllers/control.coffee
@@ -158,8 +158,7 @@ LYT.control =
             log.message "Control: bookDetails: failed with error #{error} and msg #{msg}"
           .done (details) ->
             LYT.render.bookDetails(details, content)
-        
-        LYT.loader.register "Loading book", process
+            content.children().show()
   
   # TODO: Move bookmarks list to separate page
   # TODO: Bookmarks and toc does not work properly after a forced refresh on the #book-index page. Needs to be fixed when force reloading the entire app.