Notalib/LYT

commit 4f7024e5ec8f2d591203e72b2dd3216574d63bb2

.change (event) -> was called each time the settings page was shown (multibel bindings)

src/controllers/control.coffee
@@ -87,6 +87,29 @@ LYT.control =
       LYT.loader.register "Adding book to bookshelf", LYT.bookshelf.add($("#add-to-bookshelf-button").attr("data-book-id"))
         .done( -> $.mobile.changePage LYT.config.defaultPage.hash )
 
+    $("#style-settings input").change (event) ->
+      target = $(event.target)
+      name = target.attr 'name'
+      val = target.val()
+      
+      style = jQuery.extend {}, (LYT.settings.get "textStyle" or {})
+
+      switch name
+        when 'font-size', 'font-family'
+          style[name] = val
+        when 'marking-color'
+          colors = val.split(';')
+          style['background-color'] = colors[0]
+          style['color'] = colors[1]
+          # TODO: use lower case just like all the other parameters
+        when 'playback-rate'
+          val = Number(val)
+          LYT.settings.set('playBackRate', val)
+          LYT.player.setPlayBackRate val
+                  
+      LYT.settings.set('textStyle', style)
+      LYT.render.setStyle()    
+
     $('#instrumentation').find('button.previous').on 'click', ->
       LYT.render.instrumentationGraph()?.previousEntry()
 
@@ -264,7 +287,7 @@ LYT.control =
         $.mobile.changePage LYT.config.default.hash
       else
         LYT.player.refreshContent()
-        LYT.render.setPageTitle LYT.i18n("Now playing") + " " + LYT.player.book.title
+        LYT.render.setPageTitle LYT.i18n("Now playing") + " " + LYT.playesettings.book.title
         $('.jp-play').focus()
 
 
@@ -423,29 +446,7 @@ LYT.control =
               if Number(val) is LYT.settings.get('playBackRate')
                 $(this).attr("checked", true).checkboxradio("refresh");
 
-        # Saving the GUI       
-        # TODO: The change handler below is being bound once for every time we
-        #       enter this page.
-        $("#style-settings input").change (event) ->
-          target = $(event.target)
-          name = target.attr 'name'
-          val = target.val()
-          
-          switch name
-            when 'font-size', 'font-family'
-              style[name] = val
-            when 'marking-color'
-              colors = val.split(';')
-              style['background-color'] = colors[0]
-              style['color'] = colors[1]
-            # TODO: use lower case just like all the other parameters
-            when 'playback-rate'
-              val = Number(val)
-              LYT.settings.set('playBackRate', val)
-              LYT.player.setPlayBackRate val
-                  
-          LYT.settings.set('textStyle', style)
-          LYT.render.setStyle()
+        
   
   profile: (type, match, ui, page, event) ->
     # Not passing params since it is currently only being used to indicate

src/controllers/player.coffee
@@ -229,8 +229,8 @@ LYT.player =
           if @getStatus().src == @currentAudio
             @gotDuration = false
             if @playAttemptCount <= LYT.config.player.playAttemptLimit
-              @el.jPlayer "setMedia", {mp3: @currentAudio}
-              @el.jPlayer "pause", @nextOffset
+              @el.jPlayer 'setMedia', {mp3: @currentAudio}
+              @el.jPlayer 'pause', @nextOffset
               @playAttemptCount = @playAttemptCount + 1 
               log.message "Player: loadedmetadata, play attempts: #{@playAttemptCount}"
             else
@@ -336,22 +336,22 @@ LYT.player =
 
     if offset?
       @nextOffset = offset
-      @el.jPlayer('pause', offset)
+      @el.jPlayer 'pause', offset
     else
       @nextOffset = null
-      @el.jPlayer('pause')
+      @el.jPlayer 'pause'
   
   # This is a public method - stops playback
   stop: ->
     log.message 'Player: stop'
     if @ready?
-      @el.jPlayer('stop')
+      @el.jPlayer 'stop'
       @playing = false
   
   getStatus: ->
     # Be cautious only read from status
     @el.data('jPlayer').status
-    
+
   # TODO: Remove our own playBackRate attribute and use the one on the jPlayer
   #       If it isn't available, there is no reason to try using it.
   setPlayBackRate: (playBackRate) ->
@@ -497,8 +497,8 @@ LYT.player =
         log.message "Player: playSegmentOffset: setMedia #{segment.audio}, setting nextOffset to #{offset}"
         @currentAudio = segment.audio
         @nextOffset   = offset
-        @el.jPlayer "setMedia", {mp3: segment.audio}
-        @el.jPlayer "load"
+        @el.jPlayer 'setMedia', {mp3: segment.audio}
+        @el.jPlayer 'load'
       else
         if @playing
           log.message "Player: playSegmentOffset: play from offset #{offset}"

commit a5ee5b7afab5a7a67b74f5d6fa355fb35cf2ba81

Fixed tiny bug introduced in commit 4f7024e5ec8f2d591203e72b2dd3216574d63bb2.

src/controllers/control.coffee
@@ -298,7 +298,7 @@ LYT.control =
         $.mobile.changePage LYT.config.default.hash
       else
         LYT.player.refreshContent()
-        LYT.render.setPageTitle LYT.i18n("Now playing") + " " + LYT.playesettings.book.title
+        LYT.render.setPageTitle LYT.i18n("Now playing") + " " + LYT.player.book.title
         $('.jp-play').focus()
 
   bookPlay: (type, match, ui, page, event) ->