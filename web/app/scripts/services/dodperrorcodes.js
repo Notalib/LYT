06807e2775b8e93ba94a178fc700bd487995e502
Morten Sjøgren <msn@nota.nu>
2014-11-17 15:34:35 +0100

    test DODPErrorCodes service

--- web/app/scripts/services/dodperrorcodes.js
+++ web/app/scripts/services/dodperrorcodes.js
@@ -59,9 +59,12 @@ angular.module( 'lyt3App' )
       },
       identifyDODPError: function( code ) {
         var match;
-        Object.keys( DODPfaultCodes ).some( function( signature ) {
+
+        Object.keys( DODPfaultCodes ).some( function( key ) {
+          var signature = DODPfaultCodes[ key ];
+          console.log( signature, signature.test( code ) );
           if ( signature.test( code ) ) {
-            match = DODPfaultCodes[ signature ];
+            match = DODPfaultCodes[ key ];
             return true;
           }
         } );
