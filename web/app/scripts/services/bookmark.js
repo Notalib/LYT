8cdbeda17c42adc697a27ea4e283f55b245f3fc7
Morten Sjøgren <msn@nota.nu>
2014-11-17 09:52:25 +0100

    Port Bookmark class

--- web/app/scripts/services/bookmark.js
+++ web/app/scripts/services/bookmark.js
@@ -1,15 +1,18 @@
 'use strict';
 
-/**
- * @ngdoc service
- * @name lyt3App.Bookmark
- * @description
- * # Bookmark
- * Factory in the lyt3App.
- */
 angular.module( 'lyt3App' )
   .factory( 'Bookmark', function( ) {
 
-    // Public API here
-    return function( ) {};
+    // This class represents a bookmark in a book - either set explicit by user
+    // or as a lastmark.
+    //
+    // Caveat emptor: Since a bookmark refers to a SMIL par (or seq) element,
+    // the attribute timeOffset is a SMIL offset, not an audio offset.
+    function Bookmark(data) {
+      ['note', 'URI', 'timeOffset'].forEach( function( key ) {
+        this[key] = data[key];
+      }, this );
+    }
+
+    return Bookmark;
   } );
